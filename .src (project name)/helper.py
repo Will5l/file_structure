61a6bd8464b7ada063f22ef00c69ac99df2459d1
Will5l <will.malloch@ucas-edu.net>
2026-02-17 09:52:53 -0700

    first commit

---
+++ .src (project name)/helper.py
@@ -0,0 +1,2 @@
+# Functions that are used all over the place in your code such as input sanitization
+# Never import any other files to this file
---
+++ .src (project name)/main.py
@@ -0,0 +1,7 @@
+# All python files go in src folder
+
+# main - is required. This is where the project is run from!
+    # Basically nothing on this page, it is just used to call other pages with functions that run the program
+    # Basic UI allowed on this page BUT it is better to have it on a seperate file
+
+# only import onto main if at all possible. Do not import multiple files onto your other pages!
